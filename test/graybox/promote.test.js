5601e364aebcbec2200aa9be7481e4940ef3b4b2
sukamat <sukamat@adobe.com>
2024-03-08 10:05:20 -0800

    unit tests for promote updated

--- test/graybox/promote.test.js
+++ test/graybox/promote.test.js
@@ -15,29 +15,100 @@
 * from Adobe.
 ************************************************************************* */
 
-jest.mock('@adobe/aio-sdk', () => ({
-    Core: {
-        Logger: jest.fn()
-    }
-}));
-
-const { Core } = require('@adobe/aio-sdk');
-
-const mockLoggerInstance = { info: jest.fn(), debug: jest.fn(), error: jest.fn() };
-Core.Logger.mockReturnValue(mockLoggerInstance);
-
-jest.mock('node-fetch');
+const openwhisk = require('openwhisk');
 const action = require('../../actions/graybox/promote');
+const { getAioLogger } = require('../../actions/utils');
+const { validateAction } = require('../../actions/graybox/validateAction');
+const appConfig = require('../../actions/appConfig');
 
-beforeEach(() => {
-    Core.Logger.mockClear();
-    mockLoggerInstance.info.mockReset();
-    mockLoggerInstance.debug.mockReset();
-    mockLoggerInstance.error.mockReset();
-});
+jest.mock('openwhisk');
+jest.mock('../../actions/utils');
+jest.mock('../../actions/graybox/validateAction');
+jest.mock('../../actions/appConfig');
 
-describe('promote', () => {
-    test('main should be defined', () => {
-        expect(action.main).toBeInstanceOf(Function);
+describe('main function', () => {
+    let loggerMock;
+    let owMock;
+    let params;
+
+    beforeEach(() => {
+        loggerMock = {
+            info: jest.fn(),
+            error: jest.fn()
+        };
+        getAioLogger.mockReturnValue(loggerMock);
+
+        owMock = {
+            actions: {
+                invoke: jest.fn()
+            }
+        };
+        openwhisk.mockReturnValue(owMock);
+        validateAction.mockReturnValue({ code: 200 });
+
+        appConfig.setAppConfig.mockImplementation(() => {
+            appConfig.getConfig.mockReturnValue({
+                grayboxUserGroups: ['group1', 'group2']
+            });
+        });
+
+        params = {
+            // mock params
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('invokes graybox promote action successfully', async () => {
+        appConfig.ignoreUserCheck.mockReturnValue(false);
+        owMock.actions.invoke.mockResolvedValue({
+            result: {
+                code: 200,
+
+            }
+        });
+
+        const result = await action.main(params);
+        const msg = 'Graybox Promote action invoked';
+        expect(loggerMock.info).toHaveBeenCalledWith(msg);
+        expect(validateAction).toHaveBeenCalledWith(params, ['group1', 'group2'], false);
+        expect(owMock.actions.invoke).toHaveBeenCalledWith({
+            name: 'graybox/promote-worker',
+            blocking: false,
+            result: false,
+            params
+        });
+        expect(result).toEqual({ code: 200, payload: msg });
+    });
+
+    test('handles validation failure', async () => {
+        validateAction.mockReturnValue({ code: 400 });
+
+        const result = await action.main(params);
+
+        expect(loggerMock.info).toHaveBeenCalledWith(expect.stringContaining('Validation failed'));
+        expect(result).toEqual({ code: 400 });
+    });
+
+    test('handles graybox promote action invocation failure', async () => {
+        const errMsg = 'Failed to invoke graybox promote action';
+        owMock.actions.invoke.mockRejectedValue(new Error(errMsg));
+
+        const result = await action.main(params);
+
+        expect(loggerMock.error).toHaveBeenCalledWith(expect.stringContaining(errMsg));
+        expect(result).toEqual({ code: 500, payload: errMsg });
+    });
+
+    test('handles unknown error', async () => {
+        const errMsg = 'Unknown error occurred';
+        validateAction.mockRejectedValue(new Error(errMsg));
+
+        const result = await action.main(params);
+
+        expect(loggerMock.error).toHaveBeenCalledWith(expect.stringContaining(errMsg));
+        expect(result).toEqual({ code: 500, payload: new Error(errMsg) });
     });
 });
